b9fa4939aa22751b03c1eb981608c5bf9f9e9c0f
DC-CC-21 <handstands1000@gmail.com>
2022-01-13 17:31:54 -0500

    setup files

---
+++ js/puzzle.js
@@ -0,0 +1,274 @@
+function Random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+function Rotate(deg) {
+  rotate(deg / (180 / Math.PI));
+}
+
+var img;
+const width = window.innerWidth;
+const height = window.innerHeight;
+function setup() {
+  var cnv = createCanvas(window.innerWidth, window.innerHeight);
+  cnv.parent("canvas");
+  loadImage(
+    "https://cdn.glitch.me/3dd806ba-9708-47f9-a7a4-0ffc8d4ae4bf/link.jpeg?v=1640428492948",
+    (IMAGE) => {
+      IMAGE.width = width / 2;
+      IMAGE.height = height / 2;
+      IMAGE.resize(width / 2, height / 2);
+      img = IMAGE;
+    }
+  );
+}
+
+var difficulty = ~~Math.sqrt(10);
+var puzzleW = width / 2;
+var puzzleH = height / 2;
+var pieceW;
+var pieceH;
+var pieces = [];
+var snapDist = 100;
+var x = 0;
+var y = 0;
+var col = 0;
+var frm = 0;
+
+class Piece {
+  constructor(self) {
+    for (let i = 0; i < Object.keys(self).length; i++) {
+      this[Object.keys(self)[i]] = self[Object.keys(self)[i]];
+    }
+
+    this.c = createGraphics(width, height);
+    this.c.image(this.img, 0, 0);
+    this.c = this.c.get(this.x, this.y, this.w, this.h);
+
+    // this.x *= 1.1;
+    // this.y *= 1.1;
+    this.origX = this.x + this.transX;
+    this.origY = this.y + this.transY;
+
+    this.x = Random(0, width - this.w);
+    this.y = Random(0, height - this.h);
+
+    this.clicked = false;
+    this.rotation = 90;
+    this.size = (this.w + this.h) / 2;
+    this.fixed = false;
+  }
+  display() {
+    if (
+      dist(this.x, this.y, this.origX, this.origY) < this.size / 4 &&
+      this.rotation === 0
+    ) {
+      fill(0, 255, 0, 50);
+    } else {
+      fill(255, 0, 0, 50);
+    }
+
+    push();
+    translate(this.x + this.w / 2, this.y + this.h / 2);
+    Rotate(this.rotation);
+    image(this.c, -this.w / 2, -this.h / 2);
+    rect(-this.w / 2, -this.h / 2, this.w, this.h);
+    pop();
+  }
+  press() {
+    if (this.fixed) {
+      return;
+    }
+    if (
+      this.Circ_to_Rot_rect(
+        { x: mouseX, y: mouseY, s: 2 },
+        {
+          x: this.x + this.w / 2,
+          y: this.y + this.h / 2,
+          w: this.w,
+          h: this.h,
+          rotation: this.rotation,
+        }
+      )
+    ) {
+      this.overlap = {
+        x: this.x - mouseX,
+        y: this.y - mouseY,
+      };
+      this.clicked = true;
+    }
+    return this.clicked;
+  }
+  drag() {
+    if (this.clicked) {
+      this.x = mouseX + this.overlap.x;
+      this.y = mouseY + this.overlap.y;
+    }
+  }
+  snap() {
+    if (
+      dist(this.x, this.y, this.origX, this.origY) < this.size / 4 &&
+      this.rotation === 0
+    ) {
+      this.x = this.origX;
+      this.y = this.origY;
+      this.fixed = true;
+    }
+  }
+  Circ_to_Rot_rect(x, x2) {
+    var dis = dist(x.x, x.y, x2.x, x2.y);
+    var angle = atan2(x.y - x2.y, x.x - x2.x);
+
+    x.x = x2.x + cos(angle - x2.rotation / (180 / Math.PI)) * dis;
+    x.y = x2.y + sin(angle - x2.rotation / (180 / Math.PI)) * dis;
+    var rectX = constrain(x.x, x2.x - x2.w / 2, x2.x + x2.w / 2);
+    var rectY = constrain(x.y, x2.y - x2.h / 2, x2.y + x2.h / 2);
+
+    return dist(rectX, rectY, x.x, x.y) < x.s / 2;
+  }
+}
+
+class Game {
+  constructor() {}
+  create() {
+    //set vars
+    pieceW = puzzleW / difficulty;
+    pieceH = puzzleH / difficulty;
+    img.width = puzzleW;
+    img.height = puzzleH;
+
+    //createPieces
+    for (let i = 0; i < difficulty; i++) {
+      for (let j = 0; j < difficulty; j++) {
+        pieces.push(
+          new Piece({
+            x: i * pieceW,
+            y: j * pieceH,
+            w: pieceW,
+            h: pieceH,
+            img: img,
+            transX: width / 4,
+            transY: height / 4,
+          })
+        );
+      }
+    }
+  }
+  display() {
+    for (var i = pieces.length - 1; i >= 0; i--) {
+      pieces[i].display();
+    }
+    push();
+    translate(x, y);
+    Rotate(col);
+    if (img) {
+      // image(img, -50, -50);
+    }
+    pop();
+
+    fill(255);
+
+    if (frameCount % 10 === 0) {
+      frm = frameRate();
+    }
+
+    text(
+      `frameRate: ${frm}
+Number of Pieces: ${pieces.length}
+${dist(pieces[0].x, pieces[0].y, pieces[0].origX, pieces[0].origY)}`,
+      10,
+      20
+    );
+  }
+}
+
+var drawOnce = 0;
+var game = new Game();
+
+function draw() {
+  background(75);
+  if (!drawOnce) {
+    if (img) {
+      game.create();
+      drawOnce = 1;
+    }
+  }
+  fill(255);
+  rect(width / 4, height / 4, width / 2, height / 2);
+  game.display();
+}
+
+function touched() {
+  return false;
+}
+
+var dragging = false;
+// touch and mouse
+function end_and_release() {
+  console.log(dragging);
+  if (!dragging) {
+    for (var i = 0; i < pieces.length; i++) {
+      if (pieces[i].clicked) {
+        pieces[i].rotation += 90;
+        if (pieces[i].rotation >= 360) {
+          pieces[i].rotation = 0;
+        }
+
+        pieces[i].snap();
+      }
+    }
+    col += 90;
+  }
+
+  for (var i = 0; i < pieces.length; i++) {
+    pieces[i].snap();
+    pieces[i].clicked = false;
+  }
+  dragging = false;
+  
+  for(var i = 0,j = 0; i < pieces.length; i ++){
+    if(pieces[i].fixed){
+      j += 1
+    }
+  }
+  if(j === pieces.length-1){
+    completed = true;
+  }
+}
+
+function mouse_touch_drag() {
+  dragging = true;
+  x = mouseX;
+  y = mouseY;
+  for (var i = 0; i < pieces.length; i++) {
+    pieces[i].drag();
+  }
+  return false;
+}
+
+//touching
+function touchMoved() {
+  return mouse_touch_drag();
+}
+
+function touchEnded() {
+  return end_and_release();
+}
+
+//mouse
+function mouseDragged() {
+  mouse_touch_drag();
+}
+
+function mouseReleased() {
+  end_and_release();
+}
+
+function mousePressed() {
+  if (!dragging) {
+    for (var i = 0; i < pieces.length; i++) {
+      if (pieces[i].press()) {
+        return;
+      }
+    }
+  }
+}
