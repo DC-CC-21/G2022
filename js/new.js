6f866ce8f30e45c508c7d33e8ef9dae6ec77f9b2
DC-CC-21 <handstands1000@gmail.com>
2022-01-22 21:31:23 -0500

    updated canvas setup

--- js/new.js
+++ js/new.js
@@ -4,6 +4,10 @@ var puzzleW;
 var puzzleH;
 var drawOnce = 0;
 var puzzleImg;
+var imageW;
+var imageH;
+var difficulty = 3;
+
 function setup(){
     width = window.innerWidth;
     height = window.innerHeight;
@@ -15,10 +19,25 @@ function setup(){
     cnv.parent('canvas')
 }
 
+class Piece{
+    constructor(self){
+        for(let i = 0; i < Object.keys(self).length; i ++){
+            this[Object.keys(self)[i]] = self[Object.keys(self)[i]]
+        }
+    }
+    display(){
+        stroke(0)
+        // fill(this.x,this.y,this.w)
+        rect(this.x,this.y,this.w,this.h)
+    }
+}
+var pieces = []
 
 function draw(){
     if(!drawOnce){
         drawOnce = 1;
+        imageW = puzzleW/difficulty
+        imageH = puzzleH/difficulty
         loadImage('./assets/beach/beach1.svg',(img)=>{
             puzzleImg = img;
         })
@@ -28,6 +47,26 @@ function draw(){
     fill(255)
     rect(0,0,puzzleW,puzzleH)
     if(puzzleImg){
-        image(puzzleImg,0,0,puzzleW,puzzleH)
+        if(!pieces.length){
+            for(var i = 0; i < puzzleW; i +=imageW){
+                for (var j = 0; j < puzzleH; j += imageH) {
+                  pieces.push(
+                    new Piece({
+                      x: i,
+                      y: j,
+                      w: imageW,
+                      h: imageH,
+                      msk: puzzleImg,
+                    })
+                  );
+                }
+            }
+        }
+        else{
+            for(var i = 0; i < pieces.length; i ++){
+                pieces[i].display()
+            }
+        }
+        // image(puzzleImg,0,0,puzzleW,puzzleH)
     }
 }
