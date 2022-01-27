652248e87bef5367f8bc315abd3691b6feeede09
DC-CC-21 <handstands1000@gmail.com>
2022-01-27 17:54:24 -0500

    set user select to none

--- js/puzzle3.js
+++ js/puzzle3.js
@@ -8,15 +8,18 @@ puzzle.style.height = canvas * 0.8 + "px";
 let puzzleW = puzzle.clientWidth;
 let puzzleH = puzzle.clientHeight;
 
-let difficulty = 25;
-console.log(window.location);
-difficulty = sq(~~Math.sqrt(difficulty));
+let difficulty = 9;
 let pieceW = puzzleW / Math.sqrt(difficulty);
 let pieceH = puzzleH / Math.sqrt(difficulty);
 let speed = 5;
 let xPos = 0;
 let yPos = 0;
-let puzzleImg = "./assets/Waterfalls/Waterfall7.svg";
+let puzzleImg = "./assets/underwater/underwater1.svg";
+let rotation = true;
+let angles = [0, 90, 180, 270];
+// puzzle.style.backgroundImage = `url(${puzzleImg})`
+// puzzle.style.backgroundSize = `${puzzleW}px ${puzzleH}px`;
+// puzzle.style.backgroundPosition = '0px 0px'
 
 function sq(num) {
   return num * num;
@@ -31,19 +34,24 @@ function createPieces() {
     let d = document.createElement("div");
     //background and attributes
     d.style.backgroundImage = `url(${puzzleImg})`;
-    d.style.backgroundSize = `${puzzleW}px ${puzzleH}px`;
+    d.style.backgroundSize = `${puzzleW}px ${puzzleH + 1}px`;
     d.style.backgroundPosition = `${-xPos * pieceW}px ${-yPos * pieceH}px`;
     d.setAttribute("class", "piece");
 
-    //width height and position
+    //width height
     d.dataset.origX = xPos * pieceW;
     d.dataset.origY = yPos * pieceH;
-    d.style.width = pieceW + "px";
+    d.style.width = pieceW - 1 + "px";
     d.style.height = pieceH + "px";
-    // d.style.left = xPos * pieceW + "px";
-    // d.style.top = yPos * pieceH + "px";
+
+    // position and rotation
     d.style.left = Math.random() * puzzleW + "px";
     d.style.top = Math.random() * puzzleH + "px";
+
+    d.style.transform = `rotate(${
+      angles[~~(Math.random() * angles.length)]
+    }deg)`;
+
     puzzle.append(d);
     xPos += 1;
     d.innerHTML = xPos + yPos * ~~Math.sqrt(difficulty);
@@ -59,46 +67,94 @@ createPieces();
 
 let element;
 let touching = false;
-window.addEventListener("mousedown", chooseElement);
-window.addEventListener("mousemove", moveElement);
-window.addEventListener("mouseup", releaseElement);
+let dragging = false;
+let overlap = { x: 0, y: 0 };
 
-window.addEventListener("touchstart", (e) => {
+//mouse events
+puzzle.addEventListener("mousedown", chooseElement);
+puzzle.addEventListener("mousemove", moveElement);
+puzzle.addEventListener("mouseup", releaseElement);
+// window.addEventListener("click", rotateElement);
+//touchevents
+puzzle.addEventListener("touchstart", (e) => {
   touching = true;
   chooseElement(e);
 });
-window.addEventListener("touchmove", moveElement);
-window.addEventListener("touchend", releaseElement);
+puzzle.addEventListener("touchmove", moveElement);
+puzzle.addEventListener("touchend", releaseElement);
 
+//choose a puzzle piece
 function chooseElement(e) {
-//   e.preventDefault();
-  console.log("click");
   element = e.target;
+  if (element.className !== "piece") {
+    return;
+  }
+  if (touching) {
+    if (e.touches) {
+      overlap.x = readPx(element.style.left) - e.touches[0].clientX;
+      overlap.y = readPx(element.style.top) - e.touches[0].clientY;
+    } else {
+      overlap.x = readPx(element.style.left) - e.clientX;
+      overlap.y = readPx(element.style.top) - e.clientY;
+    }
+  } else {
+    overlap.x = readPx(element.style.left) - e.clientX;
+    overlap.y = readPx(element.style.top) - e.clientY;
+  }
+
   element.style.zIndex = "1";
   console.log(element.innerHTML);
-  return false
+  return false;
 }
+//drag a puzzle piece
 function moveElement(e) {
-//   e.preventDefault();
+  if (e.target.className !== "piece") {
+    return;
+  }
+
+  dragging = true;
   if (element) {
     if (touching) {
-      element.style.left = e.touches[0].clientX + "px";
-      element.style.top = e.touches[0].clientY + "px";
+      element.style.left = e.touches[0].clientX + overlap.x + "px";
+      element.style.top = e.touches[0].clientY + overlap.y + "px";
     } else {
-      element.style.left = e.clientX + "px";
-      element.style.top = e.clientY + "px";
+      element.style.left = e.clientX + overlap.x + "px";
+      element.style.top = e.clientY + overlap.y + "px";
     }
   }
-  return false
+  return false;
 }
+//rotate
+function rotateElement(e) {
+  if (e.target.className !== "piece") {
+    return;
+  }
+
+  let original = e.target.style.transform;
+  original = Number(original.replace(/rotate\(|(deg)|\)$/gm, "")) + 90;
+  if (original >= 360) {
+    original = 0;
+  }
+
+  e.target.style.transform = `rotate(${original}deg)`;
+  return false;
+}
+//release
 function releaseElement(e) {
-//   e.preventDefault();
+  if (e.target.className !== "piece") {
+    return;
+  }
+
+  if (!dragging) {
+    rotateElement(e);
+    // return false;
+  }
   element.style.zIndex = "0";
   snapPiece();
-
   element = false;
-//   return false
+  dragging = false;
 }
+
 function readPx(value) {
   return Number(value.replace("px", ""));
 }
@@ -113,5 +169,4 @@ function snapPiece() {
     element.style.left = origX + "px";
     element.style.top = origY + "px";
   }
-  console.log(readPx(element.style.left));
 }
