0db43f6f9457dcfbd8f1941ad9f60341178ae2cf
DC-CC-21 <handstands1000@gmail.com>
2022-08-06 15:27:27 -0400

    formatted code

--- Dots/settings.js
+++ Dots/settings.js
@@ -1,16 +1,16 @@
-let select = document.querySelector('.gridSize');
+let select = document.querySelector(".gridSize");
 
-for (let i = 3; i < 16; i ++){
-  let option = document.createElement('option');
-  console.log('💩')
+for (let i = 3; i < 16; i++) {
+  let option = document.createElement("option");
+  console.log("💩");
 
   option.innerHTML = `${i} x ${i}`;
-  console.log(option)
+  console.log(option);
   select.append(option);
 }
 
-document.querySelector('.play').addEventListener('click', _ => {
-    console.log(window.location)
-    // window.location.href = 'index.html'
-    window.location.href = (window.location.origin);
-})
+document.querySelector(".play").addEventListener("click", (_) => {
+  console.log(window.location);
+  // window.location.href = 'index.html'
+  window.location.href = window.location.origin;
+});
